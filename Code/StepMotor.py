b02a8870a3675c3705eca6526eb5986c7088671d
Tom Heale <tah20112@gmail.com>
2016-02-20 19:42:34 +0000

    Both Motors Work

--- Code/StepMotor.py
+++ Code/StepMotor.py
@@ -11,14 +11,19 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17,18,27,22]
+RightStepPins = [17,18,27,22]
+LeftStepPins = [23, 24, 25, 5]
  
 # Set all pins as output
-for pin in StepPins:
-  print "Setup pins"
+for pin in RightStepPins:
   GPIO.setup(pin,GPIO.OUT)
   GPIO.output(pin, False)
  
+# Set all pins as output
+for pin in LeftStepPins:
+  GPIO.setup(pin,GPIO.OUT)
+  GPIO.output(pin, False)
+
 # Define advanced sequence
 # as shown in manufacturers datasheet
 Seq = [[1,0,0,1],
@@ -31,7 +36,7 @@ Seq = [[1,0,0,1],
        [0,0,0,1]]
         
 StepCount = len(Seq)
-StepDir = 1 # Set to 1 or 2 for clockwise
+StepDir = 2 # Set to 1 or 2 for clockwise
             # Set to -1 or -2 for anti-clockwise
  
 # Read wait time from command line
@@ -50,12 +55,15 @@ while True:
   print Seq[StepCounter]
  
   for pin in range(0,4):
-    xpin=StepPins[pin]# Get GPIO
+    xpin=RightStepPins[pin]# Get GPIO
+    ypin=LeftStepPins[pin]# Get GPIO
     if Seq[StepCounter][pin]!=0:
       print " Enable GPIO %i" %(xpin)
       GPIO.output(xpin, True)
+      GPIO.output(ypin, True)
     else:
       GPIO.output(xpin, False)
+      GPIO.output(ypin, False)
  
   StepCounter += StepDir
  
